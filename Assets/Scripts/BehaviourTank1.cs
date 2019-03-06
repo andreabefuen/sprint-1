f220e939d9fd2b8b704d5d8628f73228ee46bccb
andreabefuen <al341488@uji.es>
2019-03-06 22:19:21 +0100

    Added flee behaviour

--- Assets/Scripts/BehaviourTank1.cs
+++ Assets/Scripts/BehaviourTank1.cs
@@ -30,6 +30,11 @@ public class BehaviourTank1 : MonoBehaviour, IBaseInterface
 
     bool isDead;
 
+    //For flee
+    Transform startTransform;
+    float multiplyBy = 5f;
+    float nextTurnTime = 2f;
+
 
 
     // Start is called before the first frame update
@@ -40,7 +45,7 @@ public class BehaviourTank1 : MonoBehaviour, IBaseInterface
 
         shootableMask = LayerMask.GetMask("Shootable");
         //timeAfterFire = 0f;
-        
+        //RunFrom();
     }
 
     // Update is called once per frame
@@ -64,6 +69,11 @@ public class BehaviourTank1 : MonoBehaviour, IBaseInterface
             Shoot();
         }
        // Shoot();
+
+       // if(Time.time > nextTurnTime)
+       // {
+       //     RunFrom();
+       // }
 
     }
 
@@ -227,5 +237,35 @@ public class BehaviourTank1 : MonoBehaviour, IBaseInterface
     public void SetSpeed(float newSpeed)
     {
         speed = newSpeed;
+    }
+
+    void RunFrom()
+    {
+        startTransform = transform;
+        //temporarily point the object to look away from the player
+        transform.rotation = Quaternion.LookRotation(transform.position - GetNearestTank().transform.position);
+
+        //Then we'll get the position on that rotation that's multiplyBy down the path (you could set a Random.range
+        // for this if you want variable results) and store it in a new Vector3 called runTo
+        Vector3 runTo = transform.position + transform.forward * multiplyBy;
+        //Debug.Log("runTo = " + runTo);
+
+        //So now we've got a Vector3 to run to and we can transfer that to a location on the NavMesh with samplePosition.
+
+        NavMeshHit hit;    // stores the output in a variable called hit
+
+        // 5 is the distance to check, assumes you use default for the NavMesh Layer name
+        NavMesh.SamplePosition(runTo, out hit, 5, 1 << NavMesh.GetNavMeshLayerFromName("Default"));
+        //Debug.Log("hit = " + hit + " hit.position = " + hit.position);
+
+        // just used for testing - safe to ignore
+        nextTurnTime = Time.time + 5;
+
+        // reset the transform back to our start transform
+        transform.position = startTransform.position;
+        transform.rotation = startTransform.rotation;
+
+        // And get it to head towards the found NavMesh position
+        navTank.SetDestination(hit.position);
     }
 }
