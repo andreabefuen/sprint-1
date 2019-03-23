3288136717bf4345ed04456d100815d5b0df6ca4
andreabefuen <al341488@uji.es>
2019-03-23 19:09:48 +0100

    GameManager update

--- Assets/MiguelAI/Game Manager/Game_Manager.cs
+++ Assets/MiguelAI/Game Manager/Game_Manager.cs
@@ -4,26 +4,31 @@ using UnityEngine;
 
 public class Game_Manager : MonoBehaviour
 {
-   public GameObject[] Tanks;
+    public GameObject[] Tanks;
     public GameObject winner;
-    int deadTanks;
+    public int deadTanks;
+
+    int aux;
 
     // Start is called before the first frame update
     void Start()
     {
         Tanks = GameObject.FindGameObjectsWithTag("tank");
-    }
+        aux = Tanks.Length - 2;
 
+    }
     // Update is called once per frame
     void Update()
     {
+        deadTanks = 0;
         for (int i = 0; i < Tanks.Length; i++)
         {
+            
             if (Tanks[i] == null)
             {
                 deadTanks++;
             }
-            if (deadTanks == 2)
+            if (deadTanks == aux)
             {
                 if (Tanks[i] != null)
                 {
